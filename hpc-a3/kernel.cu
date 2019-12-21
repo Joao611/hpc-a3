55897cc1324cf922180a3f83cdf90ea8fabe99b1
Joao611 <joaofvde@gmail.com>
2019-12-21 04:39:53 +0000

    Finally implement local PSRS holy shit dudes

--- hpc-a3/kernel.cu
+++ hpc-a3/kernel.cu
@@ -3,13 +3,14 @@
 #include <stdlib.h> // random
 #include <sstream>
 #include <string>
+#include <algorithm>
 #include "assert.h"
 #include "mpi.h"
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
 #define PRINT_INTERVAL 10000
-#define LINE_SEPARATOR "-------------------------------------\n"
+#define LINE_SEPARATOR "-------------------------------------"
 
 static int rank = -1;
 static int numProcs = -1;
@@ -55,6 +56,15 @@ inline unsigned int genRandomNumber(unsigned int lowBound, unsigned int highBoun
 uint64_t closestLowerPowerOf2(uint64_t n) {
 	uint64_t p = log2(n);
 	return pow(2, p);
+}
+
+uint64_t closestHigherPowerOf2(uint64_t n) {
+	uint64_t p = log2(n);
+	if (pow(2, p) == n) {
+		return pow(2, p);
+	} else {
+		return pow(2, p + 1);
+	}
 }
 
 template<typename T>
@@ -160,7 +170,7 @@ void init(int *argc, char **argv[]) {
 		std::cout << "Execution mode: " << (execMode == GPU ? "GPU\n" : "CPU\n");
 		std::cout << "Array size: 2^" << exponent << " = " << globalN << "\n";
 		std::cout << "Base seed: 0x" << std::hex << baseSeed << std::dec << "\n";
-		std::cout << LINE_SEPARATOR;
+		std::cout << LINE_SEPARATOR << std::endl;
 	}
 
 	assertInputConditions(globalN);
@@ -168,15 +178,15 @@ void init(int *argc, char **argv[]) {
 }
 
 void printResults(unsigned int *a, uint64_t n, const double startTime, const double localProcTime, const double endTime) {
-	std::cout << LINE_SEPARATOR;
+	std::cout << LINE_SEPARATOR << "\n";
 	std::cout << "Sorted array:\n";
 	for (int i = 0; i < n; i += PRINT_INTERVAL) {
 		std::cout << a[i] << " ";
 	}
-	std::cout << "\n" << LINE_SEPARATOR;
+	std::cout << "\n" << LINE_SEPARATOR << "\n";
 	std::cout << "Local (P0) processing time: " << localProcTime - startTime << "s\n";
 	std::cout << "Global processing time: " << endTime - startTime << "s\n";
-	std::cout << LINE_SEPARATOR;
+	std::cout << LINE_SEPARATOR << std::endl;
 }
 
 
@@ -194,7 +204,7 @@ void sortPsrsChunksGPU(unsigned int *localA, uint64_t localN, const PsrsData &ps
 	const uint64_t chunkSizeBytes = psrsData.chunkSize * sizeof(unsigned int);
 	gpuErrChk(cudaMalloc(&chunkGPU, chunkSizeBytes));
 	for (uint64_t chunkStart = 0; chunkStart < localN; chunkStart += psrsData.chunkSize) {
-		unsigned int *chunkStartCPU = localA + chunkStart * sizeof(unsigned int);
+		unsigned int *chunkStartCPU = localA + chunkStart;
 		gpuErrChk(cudaMemcpy(chunkGPU, chunkStartCPU, chunkSizeBytes, cudaMemcpyHostToDevice));
 		localBitonicSortGPU(chunkGPU, psrsData.chunkSize);
 		gpuErrChk(cudaMemcpy(chunkStartCPU, chunkGPU, chunkSizeBytes, cudaMemcpyDeviceToHost));
@@ -203,17 +213,176 @@ void sortPsrsChunksGPU(unsigned int *localA, uint64_t localN, const PsrsData &ps
 }
 
 void createPsrsSample(unsigned int *sample, unsigned int *localA, const uint64_t localN, const PsrsData &psrsData) {
+	const uint64_t samplesPerChunk = psrsData.numChunks;
 	for (uint64_t chunk = 0; chunk < psrsData.numChunks; chunk++) {
 		for (uint64_t i = 0; i < psrsData.chunkSize; i += psrsData.sampleInterval) {
-			sample[chunk + i / psrsData.sampleInterval] = localA[chunk * psrsData.chunkSize + i];
+			sample[chunk * samplesPerChunk + i / psrsData.sampleInterval] = localA[chunk * psrsData.chunkSize + i];
 		}
 	}
 }
 
-void pickPivots(unsigned int *pivots, unsigned int *sample, const PsrsData psrsData, const uint64_t localN) {
+void sortPsrsSampleGPU(unsigned int *sample, const PsrsData &psrsData) {
+	unsigned int *sampleGPU = nullptr;
+	const uint64_t sampleSizeBytes = psrsData.sampleSize * sizeof(unsigned int);
+	gpuErrChk(cudaMalloc(&sampleGPU, sampleSizeBytes));
+	gpuErrChk(cudaMemcpy(sampleGPU, sample, sampleSizeBytes, cudaMemcpyHostToDevice));
+
+	localBitonicSortGPU(sampleGPU, psrsData.sampleSize);
+
+	gpuErrChk(cudaMemcpy(sample, sampleGPU, sampleSizeBytes, cudaMemcpyDeviceToHost));
+	gpuErrChk(cudaFree(sampleGPU));
+}
+
+void pickPivots(unsigned int *pivots, const unsigned int *sample, const PsrsData &psrsData, const uint64_t localN) {
 	const uint64_t p = psrsData.numChunks / 2;
 	for (int i = 0; i < psrsData.numPivots; i++) {
-		pivots[i] = sample[psrsData.numChunks * (i + 1) + p];
+		pivots[i] = sample[psrsData.numChunks * (i + 1) + p - 1];
+	}
+}
+
+/**
+ * Partitions as a matrix of indexes. One line for each chunk,
+ * where each element is the index of the first element of the next partition.
+ */
+void partitionPsrsChunks(uint64_t **partitions, const unsigned int *pivots, const unsigned int *localA, const PsrsData &psrsData) {
+	for (uint64_t chunk = 0; chunk < psrsData.numChunks; chunk++) {
+		partitions[chunk] = new uint64_t[psrsData.numPivots];
+		const unsigned int *chunkStart = localA + chunk * psrsData.chunkSize;
+		const unsigned int *chunkEnd = localA + (chunk + 1) * psrsData.chunkSize;
+		for (unsigned int pivotInd = 0; pivotInd < psrsData.numPivots; pivotInd++) {
+			auto it = std::upper_bound(chunkStart, chunkEnd, pivots[pivotInd]);
+			//if (it != chunkEnd) {
+				const uint64_t ind = std::distance(chunkStart, it);
+				partitions[chunk][pivotInd] = ind;
+			//} else { // pivot is at the end of chunk
+
+			//}
+		}
+	}
+}
+
+uint64_t calcPartitionSize(const uint64_t partition, const uint64_t chunk, uint64_t **partitions, const PsrsData &psrsData) {
+	uint64_t startPartInd = 0, nextPartInd = 0;
+	if (partition == 0) { // first partition of old chunk
+		startPartInd = 0;
+		nextPartInd = partitions[chunk][partition];
+	} else if (partition == psrsData.numChunks - 1) { // last partition of old chunk
+		startPartInd = partitions[chunk][partition - 1];
+		nextPartInd = psrsData.chunkSize;
+	} else {
+		startPartInd = partitions[chunk][partition - 1];
+		nextPartInd = partitions[chunk][partition];
+	}
+	return nextPartInd - startPartInd;
+}
+
+uint64_t calcNewChunkSize(const uint64_t newChunk, uint64_t **partitions, const PsrsData &psrsData) {
+	uint64_t sum = 0;
+	for (uint64_t chunk = 0; chunk < psrsData.numChunks; chunk++) {
+		sum += calcPartitionSize(newChunk, chunk, partitions, psrsData);
+	}
+	return sum;
+}
+
+/**
+ * Partitions are conceptually reorganized into "new chunks".
+ */
+//void normalizePsrsPartitions(uint64_t **partitions, const unsigned int *localA, const PsrsData &psrsData) {
+//	for (uint64_t newChunk = 0; newChunk < psrsData.numChunks; newChunk++) {
+//		uint64_t newChunkSize = calcNewChunkSize(0, partitions, psrsData);
+//		// Only one of the two following while's will be entered.
+//		while (newChunkSize < psrsData.chunkSize) {
+//			moveToPartitions(newChunk, partitions, psrsData);
+//			newChunkSize++;
+//		}
+//		while (newChunkSize > psrsData.chunkSize) {
+//			moveFromPartitions(newChunk, partitions, psrsData);
+//			newChunkSize--;
+//		}
+//	}
+//}
+
+//void reorderPsrsPartitions(uint64_t **partitions, const unsigned int *localA, const PsrsData &psrsData) {
+//	unsigned int *swapTemp = new unsigned int[];
+//	for (uint64_t chunk1 = 0; chunk1 < psrsData.numChunks; chunk1++) {
+//		for (uint64_t partition1 = chunk1 + 1; partition1 < psrsData.numChunks; partition1++) {
+//			uint64_t chunk2 = partition1;
+//			uint64_t partition2 = chunk1;
+//			//swapPsrsPartitions(partition1, chunk1, partition2, chunk2, swapTemp, partitions, localA);
+//		}
+//	}
+//	delete[] swapTemp;
+//}
+
+//void sortPsrsNewChunksGPU(unsigned int *localA, uint64_t localN, uint64_t **partitions, const PsrsData &psrsData) {
+//	unsigned int *chunkGPU = nullptr;
+//	const uint64_t chunkSizeBytes = psrsData.chunkSize * sizeof(unsigned int);
+//	gpuErrChk(cudaMalloc(&chunkGPU, chunkSizeBytes));
+//	for (uint64_t chunk = 0; chunk < localN; chunk++) {
+//		uint64_t chunkStart = chunk * psrsData.chunkSize;
+//		unsigned int *chunkStartCPU = localA + chunkStart;
+//		uint64_t pastPartitionsSize = 0;
+//		for (uint64_t partition = 0; partition < psrsData.numChunks; partition++) {
+//			uint64_t partitionSize = calcPartitionSize(partition, chunk, partitions, psrsData);
+//			gpuErrChk(cudaMemcpy(chunkGPU + pastPartitionsSize, chunkStartCPU + pastPartitionsSize, partitionSize, cudaMemcpyHostToDevice));
+//			pastPartitionsSize += partitionSize;
+//		}
+//		localBitonicSortGPU(chunkGPU, psrsData.chunkSize);
+//		gpuErrChk(cudaMemcpy(chunkStartCPU, chunkGPU, chunkSizeBytes, cudaMemcpyDeviceToHost));
+//	}
+//	gpuErrChk(cudaFree(chunkGPU));
+//}
+
+struct NewChunk {
+	unsigned int *chunk;
+	uint64_t powerN;
+	uint64_t realN;
+	unsigned int dummyElem;
+};
+
+void mergePartitions(NewChunk *newChunks, uint64_t **partitions, unsigned int *localA, const PsrsData &psrsData) {
+	for (uint64_t newChunk = 0; newChunk < psrsData.numChunks; newChunk++) {
+		uint64_t newChunkSize = calcNewChunkSize(newChunk, partitions, psrsData);
+		uint64_t newChunkPowerSize = closestHigherPowerOf2(newChunkSize);
+		newChunks[newChunk].chunk = new unsigned int[newChunkPowerSize];
+		newChunks[newChunk].realN = newChunkSize;
+		newChunks[newChunk].powerN = newChunkPowerSize;
+		unsigned int maxVal = 0;
+		uint64_t pastPartitionSizes = 0;
+		for (uint64_t chunk = 0; chunk < psrsData.numChunks; chunk++) {
+			uint64_t partitionSize = calcPartitionSize(newChunk, chunk, partitions, psrsData);
+			unsigned int *partitionStart = localA + chunk * psrsData.chunkSize + (newChunk > 0 ? partitions[chunk][newChunk - 1] : 0);
+			unsigned int *partitionEnd = partitionStart + partitionSize;
+			maxVal = std::max(maxVal, *(partitionEnd - 1));
+			std::copy(partitionStart, partitionEnd, newChunks[newChunk].chunk + pastPartitionSizes);
+			pastPartitionSizes += partitionSize;
+		}
+		newChunks[newChunk].dummyElem = maxVal;
+		std::fill(&(newChunks[newChunk].chunk[newChunkSize]), &(newChunks[newChunk].chunk[newChunkPowerSize]), maxVal);
+	}
+}
+
+void sortPsrsNewChunksGPU(NewChunk *newChunks, const PsrsData &psrsData) {
+	for (uint64_t newChunk = 0; newChunk < psrsData.numChunks; newChunk++) {
+		unsigned int *chunkGPU = nullptr;
+		const uint64_t chunkSizeBytes = newChunks[newChunk].powerN * sizeof(unsigned int);
+		std::cout << "Chunk size bytes = " << chunkSizeBytes << std::endl; // TODO REMOVE
+		gpuErrChk(cudaMalloc(&chunkGPU, chunkSizeBytes));
+
+		gpuErrChk(cudaMemcpy(chunkGPU, newChunks[newChunk].chunk, chunkSizeBytes, cudaMemcpyHostToDevice));
+		localBitonicSortGPU(chunkGPU, newChunks[newChunk].powerN);
+		gpuErrChk(cudaMemcpy(newChunks[newChunk].chunk, chunkGPU, chunkSizeBytes, cudaMemcpyDeviceToHost));
+
+		gpuErrChk(cudaFree(chunkGPU));
+	}
+}
+
+void movePsrsNewChunksToOutput(NewChunk *newChunks, unsigned int *localA, const PsrsData &psrsData) {
+	uint64_t pastNewChunkSizes = 0;
+	for (uint64_t newChunk = 0; newChunk < psrsData.numChunks; newChunk++) {
+		unsigned int *newChunkEnd = newChunks[newChunk].chunk + newChunks[newChunk].realN;
+		std::copy(newChunks[newChunk].chunk, newChunkEnd, localA + pastNewChunkSizes);
+		pastNewChunkSizes += newChunks[newChunk].realN;
 	}
 }
 
@@ -224,7 +393,7 @@ void pickPivots(unsigned int *pivots, unsigned int *sample, const PsrsData psrsD
 void localPsrsGPU(unsigned int *localA, uint64_t localN) {
 	uint64_t localVram = 0;
 	gpuErrChk(cudaMemGetInfo(&localVram, nullptr));
-	const uint64_t localEffVram = closestLowerPowerOf2(localVram);
+	const uint64_t localEffVram = localN * sizeof(unsigned int) / 4; //closestLowerPowerOf2(localVram) / 2;
 
 	PsrsData psrsData(localN, localEffVram);
 	unsigned int *sample = new unsigned int[psrsData.sampleSize];
@@ -232,11 +401,48 @@ void localPsrsGPU(unsigned int *localA, uint64_t localN) {
 
 	sortPsrsChunksGPU(localA, localN, psrsData);
 	createPsrsSample(sample, localA, localN, psrsData);
-	localBitonicSortGPU(sample, psrsData.sampleSize);
+	sortPsrsSampleGPU(sample, psrsData);
 	pickPivots(pivots, sample, psrsData, localN);
+	uint64_t **partitions = new uint64_t*[psrsData.numChunks];
+	partitionPsrsChunks(partitions, pivots, localA, psrsData);
+	NewChunk *newChunks = new NewChunk[psrsData.numChunks];
+	mergePartitions(newChunks, partitions, localA, psrsData);
+	sortPsrsNewChunksGPU(newChunks, psrsData);
+	movePsrsNewChunksToOutput(newChunks, localA, psrsData);
 
+	for (uint64_t chunk = 0; chunk < psrsData.numChunks; chunk++) {
+		delete[] newChunks[chunk].chunk;
+		delete[] partitions[chunk];
+	}
+	delete[] newChunks;
+	delete[] partitions;
 	delete[] pivots;
 	delete[] sample;
+}
+
+void sortEntireArrayGPU(unsigned int *localA, const uint64_t localN) {
+	unsigned int *chunkGPU = nullptr;
+	const uint64_t localBytes = localN * sizeof(unsigned int);
+	gpuErrChk(cudaMalloc(&chunkGPU, localBytes));
+	gpuErrChk(cudaMemcpy(chunkGPU, localA, localBytes, cudaMemcpyHostToDevice));
+	localBitonicSortGPU(chunkGPU, localN);
+	gpuErrChk(cudaMemcpy(localA, chunkGPU, localBytes, cudaMemcpyDeviceToHost));
+	gpuErrChk(cudaFree(chunkGPU));
+}
+
+bool fitsInVram(const uint64_t localN) {
+	uint64_t localVram = 0;
+	gpuErrChk(cudaMemGetInfo(&localVram, nullptr));
+	const uint64_t localBytes = localN * sizeof(unsigned int);
+	return localBytes <= localVram;
+}
+
+void localSortGPU(unsigned int *localA, const uint64_t localN) {
+	if (false) { //fitsInVram(localN)) {
+		sortEntireArrayGPU(localA, localN);
+	} else {
+		localPsrsGPU(localA, localN);
+	}
 }
 
 void seqBitonicMerge(unsigned int *a, uint64_t i, uint64_t n, bool dir) {
@@ -283,9 +489,7 @@ int main(int argc, char *argv[]) {
 		globalA = new unsigned int[globalN];
 	}
 
-	std::cout << "P" << rank << ": Allocating "
-		<< (execMode == GPU ? "unified" : "system")
-		<< " memory with " << localN << " elements" << std::endl;
+	std::cout << "P" << rank << ": Allocating memory with " << localN << " elements" << std::endl;
 
 	// Allocate local memory in CPU
 	localA = new unsigned int[localN];
@@ -310,7 +514,7 @@ int main(int argc, char *argv[]) {
 	// Calculate parts of array in each node
 	switch (execMode) {
 	case GPU:
-		localPsrsGPU(localA, localN);
+		localSortGPU(localA, localN);
 		break;
 	case CPU:
 		localSortCPU(localA, localN);
